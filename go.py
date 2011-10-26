1ef81153395aa90743d365d8fb5396895b22e9b4
Michael Bebenita <mbebenita@mbebenita.(none)>
2011-10-26 09:30:52 -0700

    Cleaned up python run script.

--- go.py
+++ go.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python
 import os;
+import sys;
+import argparse;
 
-os.system("make -C Show");
-os.system("Show/Show Play/play.js Play/util.js Play/bits.js Play/common.js Play/nal.js")
+commands = ["build", "run"]
+parser = argparse.ArgumentParser(description='Broadway Command Center')
+parser.add_argument('command', help="Possible commands: [%s]" % ", ".join(commands));
+
+args = parser.parse_args()
+
+if args.command not in commands:
+    print args.command + " is not a valid command."
+
+if args.command == "build":
+    os.chdir("Show")
+    os.system("scons");
+    os.chdir("..")
+
+if args.command == "run":
+    os.system("Show/bin/player Play/play.js Play/util.js Play/bits.js Play/common.js Play/nal.js Play/shell.js")
+
+sys.exit(0);
