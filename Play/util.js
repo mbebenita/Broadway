009639068bcd65611a1e87e91ac4fde4dcabb1e8
Michael Bebenita <mbebenita@mbebenita.(none)>
2011-11-10 17:45:34 -0800

    Factored out WebGLCanvas code, fixed NPOT textures, somewhat faster WebGL drawing.

--- Play/util.js
+++ Play/util.js
@@ -49,13 +49,13 @@ function getPropertyByValue(object, value) {
     return null;
 }
 
-function log(msg) {
-    print(msg);
+function log(message) {
+    console.log(message);
 }
 
-function warn(msg) {
+function warn(message) {
     if (verbosity >= WARNINGS) {
-        log('Warning: ' + msg);
+        log('Warning: ' + message);
     }
 }
 
@@ -77,27 +77,21 @@ function unexpected() {
     error("unexpected");
 }
 
-function error(msg) {
-    log(backtrace());
-    throw new Error(msg);
+function error(message) {
+  console.error(message);
+  console.trace();
 }
 
-function todo(msg) {
-    if (verbosity >= TODOS) {
-        log('TODO: ' + msg);
-    }
+function assert(condition, message) {
+  if (!condition) {
+    error(message);
+  }
 }
 
-function assert(cond, msg) {
-    if (!cond) {
-        error(msg);
-    }
-}
-
-function assertFalse(cond, msg) {
-    if (cond) {
-        error(msg);
-    }
+function assertFalse(condition, message) {
+  if (condition) {
+      error(message);
+  }
 }
 
 function assertRange(value, min, max) {
@@ -220,4 +214,53 @@ function printArray(uint8View) {
 
 function traceln(s) {
     println(s);
+}
+
+function isPowerOfTwo(x) {
+  return (x & (x - 1)) == 0;
+}
+
+/**
+ * Rounds up to the next highest power of two.
+ */
+function nextHighestPowerOfTwo(x) {
+  --x;
+  for (var i = 1; i < 32; i <<= 1) {
+      x = x | x >> i;
+  }
+  return x + 1;
+}
+
+/**
+ * Represents a 2-dimensional size value. 
+ */
+var Size = (function size() {
+  function constructor(w, h) {
+    this.w = w;
+    this.h = h;
+  }
+  constructor.prototype = {
+    toString: function () {
+      return "(" + this.w + ", " + this.h + ")";
+    },
+    getNextHighestPowerOfTwo: function() {
+      return new Size(nextHighestPowerOfTwo(this.w), 
+                      nextHighestPowerOfTwo(this.h));
+    }
+  }
+  return constructor;
+})();
+
+/**
+ * Creates a new prototype object derived from another objects prototype along with a list of additional properties.
+ *
+ * @param base object whose prototype to use as the created prototype object's prototype
+ * @param properties additional properties to add to the created prototype object
+ */
+function inherit(base, properties) {
+  var prot = Object.create(base.prototype);
+  for (var p in properties) {
+    prot[p] = properties[p];
+  }
+  return prot;
 }
