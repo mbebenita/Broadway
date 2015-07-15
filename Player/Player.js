49969d367fb5a3e687f06ab9e4cdd50a4e79fdd6
soliton4 <matthias.behrens@gmail.com>
2015-07-15 19:23:58 +0200

    background color

--- Player/Player.js
+++ Player/Player.js
@@ -257,7 +257,7 @@ p.decode(<binary>);
       obj.canvas = document.createElement('canvas');
       obj.canvas.width = width;
       obj.canvas.height = height;
-      obj.canvas.style.backgroundColor = "#333333";
+      obj.canvas.style.backgroundColor = "#0D0E1B";
       
       
       return obj;
